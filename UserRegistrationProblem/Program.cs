4db5a0833acd4460f1db32b5ff55e60edd32d720
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-11 21:53:32 +0530

    [Bharath] Add: Validation Method For LastName Implemented

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -6,11 +6,18 @@ namespace UserRegistrationProblem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To User Registration Problem\n" +
+                "Please Fill Below Details..");
             Validation validation = new Validation();
             Console.Write("Enter FirstName: ");
             string firstName = Console.ReadLine();
-            bool FNresult = validation.ValidateFirstName(firstName);
-            validation.Result(FNresult);
+            bool fNresult = validation.ValidateFirstName(firstName);
+            validation.Result(fNresult);
+
+            Console.Write("Enter LastName: ");
+            string lastName = Console.ReadLine();
+            bool lNresult = validation.ValidateLastName(lastName);
+            validation.Result(lNresult);
         }
     }
 }
--- UserRegistrationProblem/Validation.cs
+++ UserRegistrationProblem/Validation.cs
@@ -8,17 +8,23 @@ namespace UserRegistrationProblem
     class Validation
     {
         public static string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public static string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
         public bool ValidateFirstName(string first)
         {
             return Regex.IsMatch(first, Regex_FirstName);
         }
 
+        public bool ValidateLastName(string last)
+        {
+            return Regex.IsMatch(last, Regex_LastName);
+        }
+
         public void Result(bool result)
         {
             if (result)
-                Console.WriteLine("Valid FirstName");
+                Console.WriteLine("**Valid**");
             else
-                Console.WriteLine("InValid FirstName");
+                Console.WriteLine("**InValid**");
         }
     }
 }
